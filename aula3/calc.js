9da5c4990fbc96f202bfbbe875c23db4f7af2345
Vitoria <vazevedo73@gmail.com>
2023-08-11 12:26:56 -0300

    iniciado calculadora

---
+++ aula3/calc.js
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * Objetivo: Fazer uma calculadora que realize todas as operações matemáticas, *
+ *           e fazer todos os tratamentos de erros                             *
+ * Data: 11/08/2023                                                            *
+ * Autor: Vitoria Azevedo da Cruz                                              *
+ * Versão: 1.0                                                                 *
+ *******************************************************************************/
+
