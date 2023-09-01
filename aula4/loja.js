6a1b981fa9448bcd6d551d7e9d5c5df9d68d48dc
Vitoria <vazevedo73@gmail.com>
2023-09-01 11:11:56 -0300

    finalizado 'loja.js'

--- aula4/loja.js
+++ aula4/loja.js
@@ -30,20 +30,34 @@ var entradaDados = readline.createInterface({
     output: process.stdout
 })
 
+////
+//Entrada de dados - VALOR DO PRODUTO
 entradaDados.question('Insira o valor do produto: \n', function(valorProduto){
     let valor = valorProduto.replace(',' , '.');
 
+    ////
+    //Entrada de dados - FORMA DE PAGAMENTO
     entradaDados.question('Qual a forma de pagamento?\n[1- À vista, 8% de desconto]  [2- À vista, no cartão, 4% de desconto]  [3- Em 2x, sem juros]  [4- Em 4x, acréscimo de 8%]\n', function(formaPagamento){
         let pagamento = formaPagamento;
 
+        ////
+        //Validação para entrada de dados apenas de NÚMEROS, e de números entre 1 E 4
         if(pagamento > 4 || pagamento < 1 || isNaN(pagamento))
             console.log('ERRO: Opção inválida!! Digite apenas as opções acima!');
+
+            ////
+            //Validação para entrada de dados VAZIA, MENOR QUE 0 e de OUTROS CARATERES
         else if(valor == '' || isNaN(valor) || valor<0){
             console.log('ERRO: Insira o valor do produto!!');
         }else{
 
+            ////
+            //Convertendo para número
             valor = Number(valor)
-            // resultado=calculoVendas.porcentagem(valorP, fPagamento);
+
+            ////
+            //"Chamando" a função 'porcentagem', do arquivo 'calculoVendas'
+            //Nos parênteses, colocar as variáveis que irão para o arquivo da função
             valorAtualizado = calculoVendas.porcentagem(valor, pagamento)
             console.log(`O valor atualizado do produto é: ${valorAtualizado}`);
         }
--- aula4/modulo/calculoVendas.js
+++ aula4/modulo/calculoVendas.js
@@ -1,3 +1,4 @@
+//Nos parênteses, colocar as variáveis que receberão as variáveis do arquivo pricipal
 function porcentagem(valorP, fPagamento){
     let valor = valorP;
     let pagamento = fPagamento;
@@ -6,30 +7,43 @@ function porcentagem(valorP, fPagamento){
 
 
     switch(pagamento){
+
+        ////
+        //Calculo de 8% DE DESCONTO
         case "1":
             valorAtualizado = valor - (valor*0.08);
             break;
 
+        ////
+        //Calculo de 4% DE DESCONTO
         case "2":
             valorAtualizado = valor - (valor*0.04)
             break;
 
+        ////
+        //Calculo de 2 PARCELAS
         case "3":
             valorAtualizado = valor / 2;
             break;
 
+        ////
+        //Calculo de 4 PERCELAS, ACRESCIDO DE 8%
         case "4":
             valor2 = valor / 4;
             valorAtualizado = valor2 + (valor2*0.08);
             break;
     }
 
+    ////
+    //Validação para caso a variável apareça como 'undefined'
     if(valorAtualizado != undefined)
         return Number(valorAtualizado).toFixed(2);
     else
         return false;
 }
 
+////
+//Exporta a função e torna ela pública para outros arquivos
 module.exports={
     porcentagem
 }
