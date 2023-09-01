8c41b2aa401c2ffeec4b47c4a5c876d03c2c64c8
Vitoria <vazevedo73@gmail.com>
2023-09-01 11:14:47 -0300

    iniciado aula 05

---
+++ aula5/app.js
@@ -0,0 +1,6 @@
+/***********************************************************
+ * Objetivo: Realizar o calculo de uma determinada tabuada *
+ * Data: 01/09/2023                                        *
+ * Autor: Vitoria Azevedo da Cruz                          *
+ * Versão 1.0                                              *
+ ***********************************************************/
