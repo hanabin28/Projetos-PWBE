2f6ec32b5601f80aae0744a7e4cdea446b4798fa
Vitoria <vazevedo73@gmail.com>
2023-09-29 08:57:07 -0300

    .

--- atividades_NodeJs/atividade2/app.js
+++ atividades_NodeJs/atividade2/app.js
@@ -53,7 +53,9 @@ entradaDeDados.question('Insira o nome do aluno: ', function (alunoNome) {
 
                                     if (nota1 < 0 || nota1 > 100 || nota2 < 0 || nota2 > 100 || nota3 < 0 || nota3 > 100 || nota4 < 0 || nota4 > 100)
                                         console.log('ERRO: Insira valores apenas entre 1 e 100!')
-                                    
+                                    else if (nota1 == "" || nota2 == "" || nota3 == "" || nota4 == "") {
+                                        console.log('ERRO: Insira TODOS os valores!!')
+                                    }
 
                                     entradaDeDados.close();
                                 })
